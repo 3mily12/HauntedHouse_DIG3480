b64e087f8ee63f4878b277ba109ba90e0da8b656
3mily12 <ecmartin2086@gmail.com>
2025-11-25 17:42:03 -0500

    Minor Modification (freeze) Implemented

--- Assets/_3DStealthGame/Scripts/PlayerMovement.cs
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -14,6 +14,11 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed = 1.0f;
     public float turnSpeed = 20f;
 
+    // Part of  minor mod (freezing from fear.) -Emily
+    public bool isPanicking;
+    public int fightPanic; 
+    public GameObject panicCanvas; 
+
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
@@ -24,10 +29,19 @@ public class PlayerMovement : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
         MoveAction.Enable();
         m_Animator = GetComponent<Animator>();
+
+        // Part of  minor mod (freezing from fear.) -Emily
+        isPanicking = false;
+        fightPanic = 0;
+        panicCanvas.SetActive(false);
+        float panicTime = Random.Range(15, 20);
+        InvokeRepeating("StartPanic", panicTime, panicTime);
     }
 
     void FixedUpdate()
     {
+        m_Animator.SetBool("IsPanicking", isPanicking);
+
         var pos = MoveAction.ReadValue<Vector2>();
 
         float horizontal = pos.x;
@@ -41,25 +55,66 @@ public class PlayerMovement : MonoBehaviour
         bool isWalking = hasHorizontalInput || hasVeritalInput;
         m_Animator.SetBool("IsWalking", isWalking);
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation(desiredForward);
-
-        m_Rigidbody.MoveRotation(m_Rotation);
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
-
-        if (isWalking)
+        if (isPanicking == true) // (all of this) is part of  minor mod (freezing from fear.) -Emily
         {
-            if (!m_AudioSource.isPlaying)
+            Debug.Log("Too scared!");
+            panicCanvas.SetActive(true);
+            isWalking = false;
+
+            if (m_AudioSource.isPlaying) {
+                m_AudioSource.Stop();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                m_AudioSource.Play();
+                if (fightPanic < 3)
+                {
+                    fightPanic += 1;
+                }
+                else {
+                    isPanicking = false;
+                    fightPanic = 0;
+
+                }
             }
         }
-        else
+        else 
         {
-            m_AudioSource.Stop();
+            panicCanvas.SetActive(false);
+
+            // Original walk script below
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+
+            m_Rigidbody.MoveRotation(m_Rotation);
+            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+
+            if (isWalking)
+            {
+                if (!m_AudioSource.isPlaying)
+                {
+                    m_AudioSource.Play();
+                }
+            }
+            else
+            {
+                m_AudioSource.Stop();
+            }
         }
     }
 
+    // Part of  minor mod (freezing from fear.) -Emily
+    public void StartPanic() {
+        if (isPanicking == false)
+        {
+            isPanicking = true;
+        }
+        else {
+            Debug.Log("Already Panicking!");
+        }
+    }
+
+    // Below is part of the door & key stuff. I couldn't get this working, don't know why.
     public void AddKey(string keyName)
     {
         m_OwnedKeys.Add(keyName);
